7974497c8691f9b5c59f3f9e7b2137bd5d195718
mosinskimatt <91233770+mosinskimatt@users.noreply.github.com>
2021-10-29 21:09:26 -0400

    Add files via upload

---
+++ Lab01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Hello World";
+
+            string name = " from Matthew Mosinski";     
+        
+            Console.WriteLine(greeting + name);
+        }
+    }
+}
