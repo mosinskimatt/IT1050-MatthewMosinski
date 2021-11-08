fff57fc57de9f2f400c86171d58c4fa1814f2319
mosinskimatt <91233770+mosinskimatt@users.noreply.github.com>
2021-11-08 16:07:38 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lab02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            //Adding together my Keno numbers.
+
+            int number1 = 3;
+            int number2 = 7;
+            int number3 = 11;
+            int number4 = 19;
+
+            int sum = number1 + number2 + number3 + number4;
+
+            Console.WriteLine(sum);
+            
+            Console.WriteLine("\n{0}\n{1}", "Hello World!" , "From Matthew Mosinski");
+
+            Console.WriteLine("\n{0}\t{1}", "Hello World!", "From Matthew Mosinski");
+
+        }
+    }
+}
