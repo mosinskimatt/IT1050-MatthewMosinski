aaae99239600373c5287c678113e05a999665032
mosinskimatt <91233770+mosinskimatt@users.noreply.github.com>
2021-11-28 19:10:33 -0500

    Add files via upload

---
+++ Lab05/Dog/Dog/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog myDog = new Dog();
+            myDog.Name = "Fido";
+
+            // To do: uncomment the following lines of code to call the methods
+            myDog.bark();
+            myDog.doTrick("Fetch");
+
+            Console.ReadLine(); //console.ReadLine was added because the program was not running without it
+        }
+    }
+}
