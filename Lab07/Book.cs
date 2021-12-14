43bc18584723e0ab6962e35084b582351b6d008c
mosinskimatt <91233770+mosinskimatt@users.noreply.github.com>
2021-12-13 21:36:06 -0500

    Add files via upload

---
+++ Lab07/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab_7_Submit
+{
+    class Book
+    {
+        private string Title;
+        private string Author;
+        private int Year;
+
+        public Book( string Title, String Author, int Year)
+        {
+            this.Title = Title;
+            this.Author = Author;
+            this.Year = Year;
+        }
+        public void Display()
+        {
+            Console.WriteLine("{0} written by {1} in {2}\n", Title, Author, Year);
+        }
+    }
+}
---
+++ Lab07/BookTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_7_Submit
+{
+    class BookTest
+    {
+        static void Main(string[] args)
+        {
+            Book myBook = new Book("Percy Jackson and The Olympians: The Lightning Thief",
+                "Rick Riordan", 2005);
+
+            myBook.Display();
+        }
+    }
+}
